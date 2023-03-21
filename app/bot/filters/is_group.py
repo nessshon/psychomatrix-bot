5934702848ac03202a196407bbdcacf5a21e7652
nessshon <shonness98@gmail.com>
2023-03-21 10:51:19 +0500

    Added check for allowed groups

--- app/bot/filters/is_group.py
+++ app/bot/filters/is_group.py
@@ -5,4 +5,7 @@ from aiogram.dispatcher.filters import BoundFilter
 class IsGroup(BoundFilter):
 
     async def check(self, message: Message) -> bool:
-        return message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]
+        groups_types = [ChatType.GROUP, ChatType.SUPERGROUP]
+        groups_allowed = [-1001918777590]
+
+        return message.chat.type in groups_types and message.chat.id in groups_allowed
