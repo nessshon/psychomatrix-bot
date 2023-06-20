17326d0f972ab219287b12029dcafed146a42815
nessshon <shonness98@gmail.com>
2023-06-20 15:23:59 +0500

    Added two channels to allowed

--- app/bot/filters/is_group.py
+++ app/bot/filters/is_group.py
@@ -6,6 +6,11 @@ class IsGroup(BoundFilter):
 
     async def check(self, message: Message) -> bool:
         groups_types = [ChatType.GROUP, ChatType.SUPERGROUP]
-        groups_allowed = [-1001918777590]
+
+        groups_allowed = [
+            -1001918777590,
+            -1001851632872,
+            -1001945991309,
+        ]
 
         return message.chat.type in groups_types and message.chat.id in groups_allowed
